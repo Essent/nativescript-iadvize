83c92605766893e3ac6249c1b6b8f156c653a107
Natasa Vukovic <n.vukovic@levi9.com>
2021-10-05 14:06:32 +0200

    Changed hash function

--- src/iadvize.android.ts
+++ src/iadvize.android.ts
@@ -2,7 +2,7 @@ import { ChatConfiguration, IAdvizeCommon } from './iadvize.common';
 import { android as androidApp } from '@nativescript/core/application';
 import { Color, ImageSource } from '@nativescript/core';
 import { Observable } from 'rxjs';
-import * as SHA256 from 'crypto-js/sha256';
+import * as SHA512 from 'crypto-js/sha512';
 
 import IAdvizeSDK = com.iadvize.conversation.sdk.IAdvizeSDK;
 import AuthenticationOption = com.iadvize.conversation.sdk.model.auth.AuthenticationOption;
@@ -40,7 +40,7 @@ export class IAdvize extends IAdvizeCommon {
       IAdvizeSDK.initiate(androidApp.nativeApp);
       this.IAdvizeSDK = Class.forName('com.iadvize.conversation.sdk.IAdvizeSDK');
 
-      IAdvizeSDK.activate(projectId, new AuthenticationOption.Simple(SHA256(userId).toString()), new GDPROption.Disabled(), new IAdvizeCallback(
+      IAdvizeSDK.activate(projectId, new AuthenticationOption.Simple(SHA512(userId).toString()), new GDPROption.Disabled(), new IAdvizeCallback(
             {
                 onSuccess(): void {
                     console.log('iAdvize[Android] activated');
